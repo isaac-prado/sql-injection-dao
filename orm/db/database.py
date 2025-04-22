a2932cc66dd45c432101f40b6cd3da4fcbcc21eb
Isaac Prado <isaac.prado@tech4h.com.br>
2025-04-22 17:35:40 -0300

    atualizando o README

--- orm/db/database.py
+++ orm/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL="postgresql://isaac:isaac@localhost/northwind"
+DATABASE_URL="postgresql://root:root@localhost/northwind"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
